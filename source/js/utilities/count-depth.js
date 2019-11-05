4521583f0b64e19e71a6ef4e1d8681d1a99f01b8
Cody Baker <cody@flickerbox.com>
2019-11-05 13:29:25 -0800

    Fix countDepth

--- source/js/utilities/count-depth.js
+++ source/js/utilities/count-depth.js
@@ -9,7 +9,7 @@ export default function countDepth( items, depth = 1 ) {
 		let children = _.get(item, 'children', [])
 
 		if(children.length > 0) {
-			const itemDepth = this.countDepth(children, depth + 1)
+			const itemDepth = countDepth(children, depth + 1)
 
 			if( itemDepth > childDepth ) {
 				childDepth = itemDepth
