97f7bd8c631c02e2ce0115f684bc11d14cc4413b
Cody Baker <cody@flickerbox.com>
2019-11-05 11:42:59 -0800

    Fix master file

--- source/js/master.js
+++ source/js/master.js
@@ -1,10 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+/*
+ * Store
+ */
+
+import store from './store'
+
 /*
  * Components
  */
 
 import _Pacnav from './components/pacnav'
+import _PacnavDesktopItem from './components/pacnav-desktop-item'
 import _PacnavDropdown from './components/pacnav-dropdown'
 import _PacnavItem from './components/pacnav-item'
+import _PacnavMobile from './components/pacnav-mobile'
+import _PacnavMobileItem from './components/pacnav-mobile-item'
+import _PacnavPlaceholder from './components/pacnav-placeholder'
+import _PacnavPlaceholderItem from './components/pacnav-placeholder-item'
 import _PacnavToggle from './components/pacnav-toggle'
 
 
@@ -12,9 +26,16 @@ export default new class Pacnav {
 
 	install(Vue, options) {
 
+		Vue.use(Vuex)
+
 		Vue.component(_Pacnav.name, _Pacnav)
+		Vue.component(_PacnavDesktopItem.name, _PacnavDesktopItem)
 		Vue.component(_PacnavDropdown.name, _PacnavDropdown)
 		Vue.component(_PacnavItem.name, _PacnavItem)
+		Vue.component(_PacnavMobile.name, _PacnavMobile)
+		Vue.component(_PacnavMobileItem.name, _PacnavMobileItem)
+		Vue.component(_PacnavPlaceholder.name, _PacnavPlaceholder)
+		Vue.component(_PacnavPlaceholderItem.name, _PacnavPlaceholderItem)
 		Vue.component(_PacnavToggle.name, _PacnavToggle)
 
 	}
