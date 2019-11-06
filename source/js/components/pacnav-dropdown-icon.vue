d3b0771bc828edf83079cecb9cdd8f358561e1dc
Cody Baker <cody@flickerbox.com>
2019-11-06 12:25:02 -0800

    Update positioning for dropdown icon

--- source/js/components/pacnav-dropdown-icon.vue
+++ source/js/components/pacnav-dropdown-icon.vue
@@ -1,8 +1,8 @@
 <template>
 	<svg :class="{ 'PacnavDropdownIcon': true, 'is-active': active }" width="14" height="10" viewBox="0 0 14 10">
 		<g fill="none" fill-rule="evenodd" stroke="#000" stroke-linecap="square" stroke-width="2">
-			<line x1="7" x2="2" y1="7" y2="2"/>
-			<line x1="7" x2="12" y1="7" y2="2"/>
+			<line x1="7" x2="2" y1="7.5" y2="2.5"/>
+			<line x1="7" x2="12" y1="7.5" y2="2.5"/>
 		</g>
 	</svg>
 </template>
